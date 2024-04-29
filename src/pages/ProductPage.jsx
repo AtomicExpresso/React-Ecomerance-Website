687da6de80fc2cafd5580a0ed3359f5090cde6c6
atomic <baileyweb04@gmail.com>
2024-04-28 21:26:28 -0400

    added more

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,20 +1,36 @@
 import {useParams} from "react-router-dom";
+import itemText from '../data/json/store-items/onSale.json'
+import keyboard from '../assets/images/shop-items/keyboard.svg'
 
+//Weird issue with react and vite, Vite dosent allow dynamically importing images yet. So i have to do this weird work around to sort out the issue. Obv, this isnt very efficient, it will work for a small project like this, but if this was a real e-comerance site with a ton of different items, this method might cause performance issues
+const ImgSearch = img => {
+  switch(img){
+    case 'keyboard':
+      return keyboard
+    break;
+  }
+}
+
+//Creates the product page dynamically
 function ProductPage(){
-  const { name, id } = useParams();
-  const post = postthis(name, id);
+  const { id } = useParams();
+  const title = itemText[id].name
+  const desc = itemText[id].productinfo[0].desc
+  const getImg = itemText[id].img
 
-  function postthis(name, id){
-    return {
-      title: `${name}`,
-      para: `ewwewewe ${id}`
-    }
+  function ItemBuilder(props){
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        <p>{props.desc}</p>
+        <img src={ImgSearch(props.img)}></img>
+      </div>
+    )
   }
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.para}</p>
+      <ItemBuilder title={title} desc={desc} img={getImg}/>
     </div>
   )
 }
